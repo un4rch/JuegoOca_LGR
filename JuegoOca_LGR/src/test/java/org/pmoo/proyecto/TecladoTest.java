c42d531c352d055c69b6fd759ce2355bd0a14d5e
Unai <Unai@DESKTOP-D0NT56U>
2021-03-28 18:16:35 +0200

    Test clases

---
+++ JuegoOca_LGR/src/test/java/org/pmoo/proyecto/TecladoTest.java
@@ -0,0 +1,29 @@
+package org.pmoo.proyecto;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TecladoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetTeclado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPedirOpcion() {
+		fail("Not yet implemented");
+	}
+
+}
