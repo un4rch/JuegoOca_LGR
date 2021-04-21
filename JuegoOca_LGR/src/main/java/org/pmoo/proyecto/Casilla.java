0a944ab388c659c7ce56c8f0f7990a1558126375
endik <endik@LAPTOP-HPMEFCGG>
2021-04-21 19:10:42 +0200

    Casilla y ListaCasillas hechos

--- JuegoOca_LGR/src/main/java/org/pmoo/proyecto/Casilla.java
+++ JuegoOca_LGR/src/main/java/org/pmoo/proyecto/Casilla.java
@@ -1,5 +1,25 @@
 package org.pmoo.proyecto;
 
-public class Casilla {
+public abstract class Casilla {
 
+	//atributos
+	private String descripcion;
+	
+	//constructora
+	public Casilla (String pDescripcion) {
+		this.descripcion = pDescripcion;
+	}
+	
+	//metodos
+	public void imprimirInfo() {
+		System.out.println(this.descripcion);
+	}
+	
+	public void cumplirMiFuncion(Ficha pFicha) {
+		this.imprimirInfo();
+	}
+	
+	protected String getDescripcion() {
+		return this.descripcion;
+	}
 }
--- JuegoOca_LGR/src/main/java/org/pmoo/proyecto/ListaCasillas.java
+++ JuegoOca_LGR/src/main/java/org/pmoo/proyecto/ListaCasillas.java
@@ -1,5 +1,31 @@
 package org.pmoo.proyecto;
 
+import java.util.ArrayList;
+
 public class ListaCasillas {
 
+	//Atributos
+	private ArrayList<Casilla> lista;
+	private static ListaCasillas miListaCasillas;
+	
+	//Constructora
+	private ListaCasillas() {
+		this.lista = new ArrayList<Casilla>();
+	}
+	
+	//Metodos
+	public static ListaCasillas getListaCasillas() {
+		if(ListaCasillas.miListaCasillas == null) {
+			ListaCasillas.miListaCasillas = new ListaCasillas();
+		}
+		return miListaCasillas;
+	}
+	
+	public void añadirCasilla(Casilla pCasilla) {
+		this.lista.add(pCasilla);
+	}
+	
+	public Casilla queHayEnPosicion(int pPosicion) {
+		return this.lista.get(pPosicion);
+	}
 }
