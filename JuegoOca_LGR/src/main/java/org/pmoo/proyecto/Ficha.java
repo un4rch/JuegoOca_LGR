f94a96cdf8e8fe1e7a4773bdf61ad8ac73afcfe5
carmela.g <carmela.g@172.20.10.2>
2021-04-22 18:39:48 +0200

    Subido código Ficha de Ruben

--- JuegoOca_LGR/src/main/java/org/pmoo/proyecto/Ficha.java
+++ JuegoOca_LGR/src/main/java/org/pmoo/proyecto/Ficha.java
@@ -1,6 +1,64 @@
 package org.pmoo.proyecto;
 
 public class Ficha {
-	// sin acabar una vez mas
+
+
+	private String nombre;
+	private boolean heGanado;
+	private int posicion;
+	private int turnosEspera;
+
+	public Ficha(String pNombre)
+	{
+		this.nombre = pNombre;
+		this.heGanado = false;
+		this.posicion = 1;
+		this.turnosEspera = 0;
+	}
+
+	public void jugar()
+	{
+		if (this.turnosEspera==0)
+		{
+			int tirada = Dado.getDado().tirarDado();
+			this.aumentarPosicion(tirada);
+			Casilla casillaAct = ListaCasillas.getListaCasillas().queHayEnPosicion(this.posicion);
+			casillaAct.cumplirMiFuncion(this);
+		}
+		else
+		{
+			this.modificarTurnosEspera(-1);
+		}
+	}	
+
+	public boolean getHeGanado()
+	{
+		return(this.heGanado);
+	}
+
+	public void sobreescribirHeGanado()
+	{
+		this.heGanado = true;
+	}
+
+	public int getPosicion()
+	{
+		return(this.posicion);
+	}
+
+	public void modificarTurnosEspera(int pTurnosEspera)
+	{
+		this.turnosEspera = this.turnosEspera + pTurnosEspera;
+	}
+
+	public void aumentarPosicion(int pDado)
+	{
+		this.posicion = this.posicion + pDado;
+	}
+
+	public void sobreescribirPosicion(int pPos)
+	{
+		this.posicion = pPos;
+	}
 
 }
